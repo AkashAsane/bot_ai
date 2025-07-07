7ba75b209fe779495bfeb32c9f1d2b7389143351
AkashAsane <akashasane4455@gmail.com>
2025-07-07 23:21:49 +0530

    test

--- src/Components/Button/button.jsx
+++ src/Components/Button/button.jsx
@@ -3,7 +3,7 @@ import styles from "./button.module.css"
 export default function Button({children,style="primary",onClick})
 {
     return(
-        <button className={`${styles.button} ${styles[style]}`}  onClick={onClick}>
+        <button type="submit" className={`${styles.button} ${styles[style]}`}  onClick={onClick}>
               {children}
         </button>
     )
